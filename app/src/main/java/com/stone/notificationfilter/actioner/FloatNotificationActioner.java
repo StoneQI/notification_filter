95c1dcc63ef7d3a79e9e0eac171c7ebf3cc7e3a9
stone--yang <xiyaozhe@163.com>
2020-05-29 12:25:57 +0800

    INIT

---
+++ app/src/main/java/com/stone/notificationfilter/actioner/FloatNotificationActioner.java
@@ -0,0 +1,4 @@
+package com.stone.notificationfilter.actioner;
+
+public class FloatNotificationActioner {
+}
---
+++ app/src/main/java/com/stone/notificationfilter/actioner/floatnotification/FloatNotificationGroup.java
@@ -0,0 +1,6 @@
+package com.stone.notificationfilter.actioner.floatnotification;
+
+import android.view.View;
+
+public class FloatNotificationGroup extends View {
+}
